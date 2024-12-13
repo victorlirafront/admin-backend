c13bfd9b9f77e00b60fe07177db8715b2127c6b3
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 18:34:10 -0300

    style: add edit and remove item styles to Card component

--- frontend/src/components/Card/Card.styled.ts
+++ frontend/src/components/Card/Card.styled.ts
@@ -3,15 +3,12 @@ import styled from 'styled-components';
 export const StyledCard = styled.div`
   padding: 20px;
   width: 400px;
-  height: 200px;
   border-radius: 8px;
   margin: 20px 10px;
   border: 1px solid #eeeeee;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   background: #fff;
   transition: 0.1s ease-in-out;
-  display: flex;
-  align-items: center;
 
   &:hover {
     border: 1px solid #06aec8;
@@ -19,28 +16,66 @@ export const StyledCard = styled.div`
     transition: 0.1s ease-in-out;
   }
 
-  .avatar-wrapper {
-    .avatar-picture {
-      border-radius: 100%;
-      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  .info-wrapper {
+    display: flex;
+    align-items: center;
+
+    .avatar-wrapper {
+      .avatar-picture {
+        border-radius: 100%;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+      }
+      margin-right: 30px;
     }
-    margin-right: 30px;
+
+    .profile-informations-wrapper {
+      .profile-informations {
+        display: flex;
+        margin-top: 10px;
+        .profile-item {
+          display: flex;
+          align-items: center;
+
+          .star-icon {
+            margin-right: 5px;
+          }
+
+          &:not(:last-child) {
+            margin-right: 10px;
+          }
+        }
+      }
+    }
   }
 
-  .profile-informations-wrapper {
-    .profile-informations {
-      display: flex;
-      margin-top: 10px;
-      .profile-item {
-        display: flex;
-        align-items: center;
+  .controls {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    margin-top: 20px;
 
-        .star-icon {
-          margin-right: 5px;
+    .button {
+      width: 40%;
+      height: 30px;
+      cursor: pointer;
+      border: none;
+      border-radius: 4px;
+      transition: 0.3s;
+      font-weight: bolder;
+      text-transform: uppercase;
+      font-size: 11px;
+
+      &.edit {
+        background: lightgreen;
+        &:hover {
+          background: #82dc82;
         }
+      }
 
-        &:not(:last-child) {
-          margin-right: 10px;
+      &.remove {
+        background: lightcoral;
+        &:hover {
+          background: #d87373;
         }
       }
     }
--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -5,23 +5,29 @@ import Image from 'next/image';
 function Card() {
   return (
     <StyledCard>
-      <div className="avatar-wrapper">
-        <Image className="avatar-picture" src={AVATAR_MAN} height={100} width={100} alt="teste" />
-      </div>
-      <div className="profile-informations-wrapper">
-        <h1>Felipe</h1>
-        <div className="profile-informations">
-          <div className="profile-item">
-            <Image className="star-icon" src={STAR_ICON} height={20} width={20} alt="teste" />
-            <p>4.2</p>
-          </div>
-          <div className="profile-item">
-            <p>Estudante</p>
-          </div>
-          <div className="profile-item">
-            <p>ES</p>
+      <div className="info-wrapper">
+        <div className="avatar-wrapper">
+          <Image className="avatar-picture" src={AVATAR_MAN} height={100} width={100} alt="teste" />
+        </div>
+        <div className="profile-informations-wrapper">
+          <h1>Felipe</h1>
+          <div className="profile-informations">
+            <div className="profile-item">
+              <Image className="star-icon" src={STAR_ICON} height={20} width={20} alt="teste" />
+              <p>4.2</p>
+            </div>
+            <div className="profile-item">
+              <p>Estudante</p>
+            </div>
+            <div className="profile-item">
+              <p>ES</p>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="controls">
+        <button className="button edit">Editar</button>
+        <button className="button remove">Deletar</button>
       </div>
     </StyledCard>
   );
