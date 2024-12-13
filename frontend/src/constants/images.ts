04cba16f50f08a736f54412f63a7884294d16a2b
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 17:40:02 -0300

    feat: add AVATAR_MAN constant with image URL

--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -1,2 +1,4 @@
 export const PAPER_PLANE_ICON = '/paper-plane.png';
 export const FAVICON = '/favicon.ico';
+export const AVATAR_MAN =
+  'https://ik.imagekit.io/Victorliradev/bossa-nova-solutions/man_H13Xy31hu.png?updatedAt=1734122241755';
