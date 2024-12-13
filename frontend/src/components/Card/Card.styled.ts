a0c137d246058e6e5f81d3602a94262b5feb07f9
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 17:11:04 -0300

    feat: add reusable card component with styling

---
+++ frontend/src/components/Card/Card.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.div`
+  padding: 20px;
+  width: 400px;
+  height: 200px;
+  border-radius: 8px;
+  margin: 20px 10px;
+  border: 1px solid #eeeeee;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  background: #fff;
+  transition: 0.1s ease-in-out;
+
+  &:hover {
+    border: 1px solid #06aec8;
+    transform: scale(1.05);
+    transition: 0.1s ease-in-out;
+  }
+`;
