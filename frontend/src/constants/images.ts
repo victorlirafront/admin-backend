6a448259000ee20d5ae92a0845c26c8187f1cbb1
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 18:15:51 -0300

    feat: add STAR_ICON constant with image URL

--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -2,3 +2,5 @@ export const PAPER_PLANE_ICON = '/paper-plane.png';
 export const FAVICON = '/favicon.ico';
 export const AVATAR_MAN =
   'https://ik.imagekit.io/Victorliradev/bossa-nova-solutions/man_H13Xy31hu.png?updatedAt=1734122241755';
+export const STAR_ICON =
+  'https://ik.imagekit.io/Victorliradev/bossa-nova-solutions/star_KIFvUb4oZ.png?updatedAt=1734122679885';
