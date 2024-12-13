e0a5cfccdfdf037a44ff0fd5d20a8cf9253d9557
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 17:13:29 -0300

    style: change footer padding

--- frontend/src/components/Footer/Footer.styled.ts
+++ frontend/src/components/Footer/Footer.styled.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   background: #06aec8;
   color: #fff;
-  padding: 10px 40px;
+  padding: 20px 40px;
   text-align: center;
   font-size: 16px;
 `;
