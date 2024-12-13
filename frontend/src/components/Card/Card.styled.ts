3e1d9f5f3a4f59ee17f0aa5a054e22de652ba5d5
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 18:15:24 -0300

    chore: add hardcoded data to test card component

--- frontend/src/components/Card/Card.styled.ts
+++ frontend/src/components/Card/Card.styled.ts
@@ -10,6 +10,8 @@ export const StyledCard = styled.div`
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   background: #fff;
   transition: 0.1s ease-in-out;
+  display: flex;
+  align-items: center;
 
   &:hover {
     border: 1px solid #06aec8;
@@ -17,8 +19,30 @@ export const StyledCard = styled.div`
     transition: 0.1s ease-in-out;
   }
 
-  .avatar-picture {
-    border-radius: 100%;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  .avatar-wrapper {
+    .avatar-picture {
+      border-radius: 100%;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+    }
+    margin-right: 30px;
+  }
+
+  .profile-informations-wrapper {
+    .profile-informations {
+      display: flex;
+      margin-top: 10px;
+      .profile-item {
+        display: flex;
+        align-items: center;
+
+        .star-icon {
+          margin-right: 5px;
+        }
+
+        &:not(:last-child) {
+          margin-right: 10px;
+        }
+      }
+    }
   }
 `;
--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,12 +1,27 @@
-import { AVATAR_MAN } from '@/constants/images';
+import { AVATAR_MAN, STAR_ICON } from '@/constants/images';
 import { StyledCard } from './Card.styled';
 import Image from 'next/image';
 
 function Card() {
   return (
     <StyledCard>
-      <div>
+      <div className="avatar-wrapper">
         <Image className="avatar-picture" src={AVATAR_MAN} height={100} width={100} alt="teste" />
+      </div>
+      <div className="profile-informations-wrapper">
+        <h1>Felipe</h1>
+        <div className="profile-informations">
+          <div className="profile-item">
+            <Image className="star-icon" src={STAR_ICON} height={20} width={20} alt="teste" />
+            <p>4.2</p>
+          </div>
+          <div className="profile-item">
+            <p>Estudante</p>
+          </div>
+          <div className="profile-item">
+            <p>ES</p>
+          </div>
+        </div>
       </div>
     </StyledCard>
   );
