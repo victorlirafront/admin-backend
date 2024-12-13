57fcef342a6484432adf351b3bd143425cb731c6
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 17:39:29 -0300

    fix: add 'ik.imagekit.io' to image domains in next.config.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['ik.imagekit.io'],
+  },
 };
 
 export default nextConfig;
