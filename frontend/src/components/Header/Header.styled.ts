0becc83e1667093833384a75e9eef23f9fcb1e43
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 16:24:11 -0300

    feat: add basic header component

--- frontend/src/components/Header/Header.styled.ts
+++ frontend/src/components/Header/Header.styled.ts
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
-export const StyledHeader = styled.header``;
+export const StyledHeader = styled.header`
+  background: #06aec8;
+  color: #fff;
+  padding: 10px 40px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .paper-plane-icon {
+    transform: rotate(8deg);
+  }
+
+  .header-menu-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+
+    .header-menu-list-item {
+      text-transform: uppercase;
+      text-decoration: none;
+      font-weight: bolder;
+      font-size: 14px;
+      cursor: pointer;
+
+      &.selected {
+        color: #fff;
+      }
+
+      color: #e3e3e3;
+
+      &:not(:last-child) {
+        margin-right: 30px;
+      }
+    }
+  }
+`;
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,23 @@
 import { StyledHeader } from '@/components/Header/Header.styled';
+import { PAPER_PLANE_ICON } from '@/constants/images';
+import Image from 'next/image';
 
 function Header() {
   return (
     <StyledHeader>
-      <h1>Header</h1>
+      <Image
+        className="paper-plane-icon"
+        src={PAPER_PLANE_ICON}
+        width={50}
+        height={50}
+        alt="teste"
+      />
+
+      <ul className="header-menu-list">
+        <li className="header-menu-list-item selected">Voluntários</li>
+        <li className="header-menu-list-item">Eventos</li>
+        <li className="header-menu-list-item">Calendário</li>
+      </ul>
     </StyledHeader>
   );
 }
