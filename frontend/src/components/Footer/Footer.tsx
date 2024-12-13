e9a482d197b7897b71cfe268954ec04f5d6c3042
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 14:33:44 -0300

    feat: add Footer component as a placeholder

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { StyledFooter } from '@/components/Footer/Footer.styled';
+
+function Footer() {
+  return (
+    <StyledFooter>
+      <h1>Footer</h1>
+    </StyledFooter>
+  );
+}
+
+export default Footer;
