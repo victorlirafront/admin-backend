f0de244f40d54bed9926f5879e422be73581ff7a
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 21:06:52 -0300

    feat: add UsersController with endpoints for fetching and creating users

--- backend/src/users/controllers/users/users.controller.ts
+++ backend/src/users/controllers/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  async getUser() {
+    const users = await this.userService.findUsers();
+    return users;
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+}
