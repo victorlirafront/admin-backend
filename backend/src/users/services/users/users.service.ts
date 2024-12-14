e0e058a7594c1e3aeae666587df3a70855750ea0
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-13 21:06:35 -0300

    feat: implement UsersService with find and create methods

--- backend/src/users/services/users/users.service.ts
+++ backend/src/users/services/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { CreateUserParams } from 'src/utils/types';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  findUsers() {
+    return this.userRepository.find();
+  }
+
+  createUser(userDetails: CreateUserParams) {
+    const newUser = this.userRepository.create({
+      ...userDetails,
+      createdAt: new Date(),
+    });
+
+    return this.userRepository.save(newUser);
+  }
+}
