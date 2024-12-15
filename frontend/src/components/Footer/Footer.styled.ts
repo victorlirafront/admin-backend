e5379f1b916f01ef6cbff51dfcfdf53cf3446b29
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-15 17:12:25 -0300

    feat: make Header component responsive

--- frontend/src/components/Footer/Footer.styled.ts
+++ frontend/src/components/Footer/Footer.styled.ts
@@ -6,4 +6,8 @@ export const StyledFooter = styled.footer`
   padding: 20px 40px;
   text-align: center;
   font-size: 16px;
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+    padding: 10px;
+  }
 `;
