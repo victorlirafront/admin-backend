bd94e399b719e954880d4517a874bb4fc9fadddf
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-15 10:11:56 -0300

    refactor: centralize environment URLs and CORS settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from '../config/config';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
-    const isDevelopment = ENVIRONMENT === 'development';
+    const { CORS_ORIGIN, SERVER_URL } = config[ENVIRONMENT];
 
     app.enableCors({
-      origin: isDevelopment
-        ? ['http://localhost:3000', 'http://localhost:3001']
-        : ['https://bossa-nova-solutions-challenge-production.up.railway.app'],
+      origin: CORS_ORIGIN,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization'],
     });
@@ -18,13 +17,7 @@ async function bootstrap() {
     const PORT = process.env.API_PORT || 3003;
     await app.listen(PORT);
 
-    console.log(
-      `Server is running on ${
-        isDevelopment
-          ? `http://localhost:${PORT}`
-          : 'https://bossa-nova-solutions-challenge-production.up.railway.app'
-      }`,
-    );
+    console.log(`Server is running on ${SERVER_URL(PORT)}`);
   } catch (error) {
     console.error('Error starting the server:', error);
     process.exit(1);
