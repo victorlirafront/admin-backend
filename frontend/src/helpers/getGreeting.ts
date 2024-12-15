c16372b114c84e89ab09bf41e7eddf2b23b2a96f
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-14 21:42:57 -0300

    feat: add greeting function based on time of day

---
+++ frontend/src/helpers/getGreeting.ts
@@ -0,0 +1,11 @@
+const getGreeting = (hour: number) => {
+  if (hour >= 6 && hour < 12) {
+    return 'Bom dia';
+  } else if (hour >= 12 && hour < 18) {
+    return 'Boa tarde';
+  } else {
+    return 'Boa noite';
+  }
+};
+
+export default getGreeting;
